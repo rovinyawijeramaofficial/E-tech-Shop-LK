c8476e02c33a5e4d6ddcf12e74674e70debd9215
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-10 16:43:22 +0530

    Updated

---
+++ components/Roadmap/Roadmap.js
@@ -0,0 +1,45 @@
+import styles from "./Roadmap.module.css";
+
+const Roadmap = () => {
+  return (
+    <div className={styles.roadmapSection}>
+      <h2 className={styles.title}>Roadmap</h2>
+      <div className={styles.timeline}>
+        <div className={`${styles.timelineItem} ${styles.left}`}>
+          <h3 className={styles.subHeadline}>Sub Headline</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            lectus nulla at volutpat diam ut venenatis tellus in.
+          </p>
+        </div>
+        <div className={`${styles.timelineItem} ${styles.right}`}>
+          <h3 className={styles.subHeadline}>Sub Headline</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            lectus nulla at volutpat diam ut venenatis tellus in.
+          </p>
+        </div>
+        <div className={`${styles.timelineItem} ${styles.left}`}>
+          <h3 className={styles.subHeadline}>Sub Headline</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            lectus nulla at volutpat diam ut venenatis tellus in.
+          </p>
+        </div>
+        <div className={`${styles.timelineItem} ${styles.right}`}>
+          <h3 className={styles.subHeadline}>Sub Headline</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            lectus nulla at volutpat diam ut venenatis tellus in.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Roadmap;
