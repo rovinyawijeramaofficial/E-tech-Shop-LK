e37993ed92b470dd485195c4ac40bdd42244afb7
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-10 16:45:30 +0530

    Updated

---
+++ pages/allreviews.js
@@ -0,0 +1,58 @@
+import ProductHeader from '../components/ProductHeader';
+import ReviewCard from '../components/ReviewCard';
+import Pagination from '../components/Pagination';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const AllReviews = () => {
+  const reviews = [
+    {
+      rating: 5,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      author: "James Franco",
+      location: "Panadura",
+      date: "23 Oct, 2022",
+    },
+    {
+      rating: 5,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      author: "James Franco",
+      location: "Panadura",
+      date: "23 Oct, 2022",
+    },
+    {
+      rating: 5,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      author: "James Franco",
+      location: "Panadura",
+      date: "23 Oct, 2022",
+    },
+    {
+      rating: 5,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+      author: "James Franco",
+      location: "Panadura",
+      date: "23 Oct, 2022",
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto mt-60 py-8 px-4">
+      <main style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
+        <ProductHeader />
+        <div style={{ marginTop: '20px' }}>
+          {reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))}
+        </div>
+        <Pagination />
+      </main>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AllReviews;
