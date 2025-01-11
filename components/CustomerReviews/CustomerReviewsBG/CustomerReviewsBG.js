e5f30cfa199677f6209ad25276cf9bd6379ac639
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-11 17:19:56 +0530

    created and updated

---
+++ components/CustomerReviews/CustomerReviewsBG/CustomerReviewsBG.js
@@ -0,0 +1,13 @@
+// CustomerReviewsBG.js
+import React from "react";
+import styles from "./CustomerReviewsBG.module.css";
+
+const CustomerReviewsBG = ({ children }) => {
+  return (
+    <div className={styles.backgroundContainer}>
+      {children}
+    </div>
+  );
+};
+
+export default CustomerReviewsBG;
