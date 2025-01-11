cb411629606963f0759e6a4c8aabadd226256ead
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-11 17:25:22 +0530

    updated

--- pages/home.js
+++ pages/home.js
@@ -1,43 +1,27 @@
-// pages/home.js
-
+import React from "react";
 import OnSaleProducts from "@/components/OnSaleProducts";
-import CustomerReviews from "@/components/CustomerReviews";
+import CustomerReviewsCard from "@/components/CustomerReviews/CustomerReviewsCard/CustomerReviewsCard";
 import FeaturedProduct from "@/components/FeaturedProduct";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import CustomerReviewsBG from "@/components/CustomerReviews/CustomerReviewsBG/CustomerReviewsBG";
 import DisplayBanner from "@/components/DisplayBanner";
 import DiscountBanner from "@/components/DiscountBanner";
 import SmartDevices from "@/components/SmartDevices";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    
+    <CustomerReviewsBG>
       <Header />
-      <div className="relative">
-        <Image
-          alt="line"
-          width={100}
-          height={30}
-          className="absolute top-0 right-0 w-fit h-[670px] object-cover z-0 opacity-5 items-end"
-        />
-
-        <Image
-          alt="line"
-          width={200}
-          height={500}
-          className="absolute top-0 right-40 w-fit h-[670px] object-cover z-0 opacity-5 items-end"
-        />
-      </div>
-
       <DisplayBanner />
       <SmartDevices />
       <FeaturedProduct />
       <OnSaleProducts />
       <DiscountBanner />
       <OnSaleProducts />
-      <CustomerReviews />
+      <CustomerReviewsCard />
       <Footer />
-    </>
+    </CustomerReviewsBG>
   );
 }
