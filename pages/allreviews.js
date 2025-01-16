d7e95701fcc3e0b8769b68eb292d85766bfb989f
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-16 17:20:51 +0530

    layouts updated

--- pages/allreviews.js
+++ pages/allreviews.js
@@ -3,8 +3,12 @@ import ReviewCard from '../components/ReviewCard';
 import Pagination from '../components/Pagination';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useRouter } from "next/router";
 
 const AllReviews = () => {
+  const router = useRouter();
+  const { id } = router.query; // Access the id from the URL query
+
   const reviews = [
     {
       rating: 5,
@@ -40,15 +44,15 @@ const AllReviews = () => {
     <>
       <Header />
       <div className="container mx-auto mt-60 py-8 px-4">
-      <main style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
-        <ProductHeader />
-        <div style={{ marginTop: '20px' }}>
-          {reviews.map((review, index) => (
-            <ReviewCard key={index} review={review} />
-          ))}
-        </div>
-        <Pagination />
-      </main>
+        <main style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
+          <ProductHeader />
+          <div style={{ marginTop: '20px' }}>
+            {reviews.map((review, index) => (
+              <ReviewCard key={index} review={review} />
+            ))}
+          </div>
+          <Pagination />
+        </main>
       </div>
       <Footer />
     </>
