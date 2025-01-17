28522c725064f116f84961465cf501211036a356
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-17 18:14:27 +0530

    updated

--- pages/blogsingle.js
+++ pages/blogsingle.js
@@ -1,71 +1,55 @@
 import React from "react";
+import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import { Poppins } from "next/font/google";
+import BlogSingleHeroSection from "@/components/BlogSingleHeroSection";
+import BlogSingleAddComment from "@/components/BlogSingleAddComment";
+import BlogSingleRelatedProducts from "@/components/BlogSingleRelatedProducts";
+import BlogSingleRelatedPosts from "@/components/BlogSingleRelatedPosts";
 
 const BlogSingle = () => {
   return (
-    <div className="blog-single">
-      {/* Blog Header Section */}
-      <header className="blog-header">
-        <img 
-          src="/path-to-header-image.jpg" 
-          alt="Blog Header" 
-          className="header-image" 
-        />
-        <div className="header-content">
-          <h1>Lorem Ipsum dolor sit amet, consectetur...</h1>
-          <p>by Matthew Rocksmith | 23 Oct 2022 | 3 Minute Read</p>
-        </div>
-      </header>
+    <>
+    <Header />
+    <div className="container mx-auto mt-60 py-8 px-4">
+    <div className="blogsingle-page">
 
-      {/* Blog Content Section */}
-      <section className="blog-content">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <div className="share-section">
-          <p>Share this article on:</p>
-          <div className="social-icons">
-            <i className="fab fa-facebook"></i>
-            <i className="fab fa-twitter"></i>
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-instagram"></i>
-          </div>
-        </div>
-      </section>
+ <BlogSingleHeroSection/>
 
-      {/* Comment Form Section */}
-      <section className="comments-section">
-        <h2>Add a Comment</h2>
-        <form className="comment-form">
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Email Address" required />
-          <textarea placeholder="Your Comment" required></textarea>
-          <button type="submit">Send</button>
-        </form>
+      {/* BlogSingle Section */}
+      <div className="blogsingle-section mt-28">
+        <div className="blogsingle-content"
+        style={{
+            height: "641px",
+            width:"898px",
+            margin: "0 auto",
+            fontFamily:"poppins",
+            fontWeight:300,
+            fontSize:"20px",
+            backgroundPosition: "center",
+            lineHeight:"29.12px",
+           
+          }}
+          >
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis lectus nulla at volutpat diam ut venenatis tellus in. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Convallis a cras semper auctor neque. Vitae aliquet nec ullamcorper sit amet risus nullam. Augue interdum velit euismod in pellentesque massa. Sit amet consectetur adipiscing elit ut aliquam purus sit. Integer enim neque volutpat ac tincidunt vitae. Facilisi etiam dignissim diam quis enim lobortis scelerisque. Libero enim sed faucibus turpis in eu mi bibendum neque. Consequat ac felis donec et odio pellentesque. Sed vulputate odio ut enim blandit. Et malesuada fames ac turpis egestas sed. Vulputate dignissim suspendisse in est ante. Ullamcorper a lacus vestibulum sed arcu non odio.
+          <br/><br/>
+Eu non diam phasellus vestibulum. Lacus sed turpis tincidunt id aliquet risus. Morbi non arcu risus quis varius quam quisque. Quam nulla porttitor massa id neque. Lectus proin nibh nisl condimentum id venenatis a. Vel turpis nunc eget lorem dolor sed viverra ipsum nunc. Porta lorem mollis aliquam ut porttitor leo a. Tristique nulla aliquet enim tortor at auctor. Nam aliquam sem et tortor consequat id. Diam vel quam elementum pulvinar etiam non quam. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Purus sit amet volutpat consequat mauris nunc congue nisi vitae. Aenean sed adipiscing diam donec adipiscing. Pellentesque habitant morbi tristique senectus et netus et. Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Posuere lorem ipsum dolor sit amet. Libero volutpat sed cras ornare arcu dui vivamus arcu. Penatibus et magnis dis parturient montes nascetur.
+          </p>
 
-        {/* Recent Comments */}
-        <div className="recent-comments">
-          <h2>Recent Comments</h2>
-          <ul>
-            <li>
-              <strong>Matthew Rocksmith:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <button className="reply-btn">Reply</button>
-            </li>
-            <li>
-              <strong>Matthew Rocksmith:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <button className="reply-btn">Reply</button>
-            </li>
-            <li>
-              <strong>Matthew Rocksmith:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <button className="reply-btn">Reply</button>
-            </li>
-          </ul>
-          <button className="load-more-btn">Load More</button>
+          <BlogSingleAddComment/> 
+          <BlogSingleRelatedProducts/> 
+          <BlogSingleRelatedPosts/> 
+          <Footer /> 
         </div>
-      </section>
+        
+      </div>
+      
     </div>
+          </div>  
+                         
+        </>
   );
 };
 
