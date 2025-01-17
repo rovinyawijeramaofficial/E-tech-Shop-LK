69cf0b11f497caf46c06077891657687c6656253
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-01-17 09:12:33 +0530

    updated

--- pages/bloghome.js
+++ pages/bloghome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from "@/components/Footer";
+import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
 import Header from "@/components/Header";
 
 const BlogHome = () => {
