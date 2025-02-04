b4f78afc6b038335fa05800786acc22baf9da8ed
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-04 20:00:07 +0530

    updated

--- components/DisplayBanner.js
+++ components/DisplayBanner.js
@@ -10,7 +10,7 @@ const DisplayBanner = () => {
   };
 
   return (
-    <div className="w-full relative overflow-hidden -mt-5">
+    <div className="w-full relative overflow-hidden -mt-11">
       {/* Desktop View */}
       <div className="hidden md:block w-full h-[850px] relative bg-black overflow-hidden">
         {/* Background Image */}
