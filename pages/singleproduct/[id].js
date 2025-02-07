fef7bcbfe947440fce668e5db3622bfa618c5b93
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 11:51:48 +0530

    updated

--- pages/singleproduct/[id].js
+++ pages/singleproduct/[id].js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Header from "@/components/Header";
-import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import Footer from "@/components/Footer";
 import Link from 'next/link';
 
 const SingleProduct = () => {
