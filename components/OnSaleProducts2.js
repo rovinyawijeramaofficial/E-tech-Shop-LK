df984f1ea9c0bee7cfbeef808154d5d7688ef29d
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 10:52:59 +0530

    updated

---
+++ components/OnSaleProducts2.js
@@ -0,0 +1,85 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import FeaturedProductCard from "./FeaturedProductCard";
+
+export default function FeaturedProduct() {
+  const products = [
+    {
+      id: 1,
+      logo: "/images/Rectangle 30.png",
+      title: "iPhone 14 Pro Max - 256GB",
+      price: "Rs.249,999",
+      originalPrice: "Rs.399,999",
+      rating: 5,
+    },
+    {
+      id: 2,
+      logo: "/images/Rectangle 30.png",
+      title: "iPhone 14 Pro Max - 256GB",
+      price: "Rs.249,999",
+      originalPrice: "Rs.399,999",
+      rating: 5,
+    },
+    {
+      id: 3,
+      logo: "/images/Rectangle 30.png",
+      title: "iPhone 14 Pro Max - 256GB",
+      price: "Rs.249,999",
+      originalPrice: "Rs.399,999",
+      rating: 5,
+    },
+    {
+      id: 4,
+      logo: "/images/Rectangle 30.png",
+      title: "iPhone 14 Pro Max - 256GB",
+      price: "Rs.249,999",
+      originalPrice: "Rs.399,999",
+      rating: 5,
+    },
+    {
+      id: 5,
+      logo: "/images/Rectangle 30.png",
+      title: "iPhone 14 Pro Max - 256GB",
+      price: "Rs.249,999",
+      originalPrice: "Rs.399,999",
+      rating: 5,
+    },
+  ];
+
+  return (
+    <div className=" hidden md:block p-4 sm:p-6 md:p-8 bg-gray-100">
+      {/* Header Section */}
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="mt-10 font-poppins font-semibold text-[24px] sm:text-[28px] text-black leading-[1.2]">
+          On Sale Products
+        </h2>
+      </div>
+
+      {/* Swiper Section */}
+      <div className="relative">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation={{ prevEl: ".custom-prev", nextEl: ".custom-next" }}
+          pagination={{ el: ".custom-pagination", clickable: true }}
+          spaceBetween={8}
+          slidesPerView={1}
+          breakpoints={{
+            430: { slidesPerView: 1.5, spaceBetween: 12 },
+            640: { slidesPerView: 2, spaceBetween: 16 },
+            1024: { slidesPerView: 4, spaceBetween: 24 },
+          }}
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product.id}>
+              <FeaturedProductCard product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
