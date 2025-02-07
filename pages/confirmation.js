aaa1524c5825ad8b7e7c62015cef1834ddb322b6
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 11:50:54 +0530

    updated

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Confirmation() {
