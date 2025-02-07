f14a97ab2203271d0dc9ac551995feecdfbc6fc9
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 11:51:02 +0530

    updated

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Header from "@/components/Header";
-import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import Footer from "@/components/Footer";
 import Newsletter from "@/components/Newsletter";
 
 const ContactUs = () => {
