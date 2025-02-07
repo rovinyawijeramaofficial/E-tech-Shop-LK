f6db431482e090fe126b6d703fad3a210618355e
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 11:50:02 +0530

    updated

--- pages/blogarchivesearch.js
+++ pages/blogarchivesearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import BlogHomePagination from "@/components/BlogHomePagination";
 
