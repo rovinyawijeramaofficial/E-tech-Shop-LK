a3068838a3b5aafdb94b9fb4b9b335c1f47fdc72
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-07 11:51:22 +0530

    updated

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Header from "@/components/Header";
-import Footer from "@/components/CustomerReviews/CustomerReviewsBG/Footer";
+import Footer from "@/components/Footer";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
