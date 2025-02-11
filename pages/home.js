e4c567990e2cea4c113f91dcd73af928a764202a
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-11 17:46:17 +0530

    updated

--- pages/home.js
+++ pages/home.js
@@ -1,30 +1,18 @@
+// Home.js (Main Component)
 import React from "react";
-import OnSaleProducts1 from "@/components/OnSaleProducts1";
-import OnSaleProducts2 from "@/components/OnSaleProducts2";
-import CustomerReviewsCard from "@/components/CustomerReviewsCard";
-import FeaturedProduct from "@/components/FeaturedProduct";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import DisplayBanner from "@/components/DisplayBanner";
-import DiscountBanner from "@/components/DiscountBanner";
-import SmartDevices from "@/components/SmartDevices";
-import CustomerReviewsBG from "@/components/CustomerReviewsBG";
+import useWindowSize from "../hooks/useWindowSize"; // Ensure correct path
+import HomeDesktop from "../components/HomeDesktop";
+import HomeMobile from "../components/HomeMobile";
 
-export default function HomePage() {
+const Home = () => {
+  const { width } = useWindowSize(); // Get the screen width
+  const breakpoint = 768; // Define breakpoint for mobile
+
   return (
     <>
-      <Header />
-      <DisplayBanner />
-      <SmartDevices />
-      <FeaturedProduct />
-  <OnSaleProducts1 />
-  <DiscountBanner />
-      <OnSaleProducts2 />
-      {/* Move CustomerReviewsBG to the bottom */}
-      <CustomerReviewsBG>
-        <CustomerReviewsCard />
-        <Footer isHomePage={true} />
-      </CustomerReviewsBG>
+      {width >= breakpoint ? <HomeDesktop /> : <HomeMobile />}
     </>
   );
-}
+};
+
+export default Home;
