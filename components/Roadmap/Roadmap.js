ecf045dbf52a51f53d3ea0632c494cb9d3e1b39f
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-14 17:26:05 +0530

    finished

--- components/Roadmap/Roadmap.js
+++ components/Roadmap/Roadmap.js
@@ -3,16 +3,10 @@ import styles from "./Roadmap.module.css";
 const Roadmap = () => {
   return (
     <div className={styles.roadmapSection}>
+      <div className="mt-5 flex justify-center">
       <h2 className={styles.title}>Roadmap</h2>
+      </div>
       <div className={styles.timeline}>
-        <div className={`${styles.timelineItem} ${styles.left}`}>
-          <h3 className={styles.subHeadline}>Sub Headline</h3>
-          <p className={styles.description}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
-            lectus nulla at volutpat diam ut venenatis tellus in.
-          </p>
-        </div>
         <div className={`${styles.timelineItem} ${styles.right}`}>
           <h3 className={styles.subHeadline}>Sub Headline</h3>
           <p className={styles.description}>
@@ -30,6 +24,14 @@ const Roadmap = () => {
           </p>
         </div>
         <div className={`${styles.timelineItem} ${styles.right}`}>
+          <h3 className={styles.subHeadline}>Sub Headline</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            lectus nulla at volutpat diam ut venenatis tellus in.
+          </p>
+        </div>
+        <div className={`${styles.timelineItem} ${styles.left}`}>
           <h3 className={styles.subHeadline}>Sub Headline</h3>
           <p className={styles.description}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
